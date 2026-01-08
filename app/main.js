e9f263d6cb70fd41fca8e9bba3667ad3931808f7
Zahdidmohd <zahidkhanavi@gmail.com>
2026-01-08 22:04:45 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -3,9 +3,30 @@ import net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  console.log("Client connected");
+  
+  connection.on("data", (data) => {
+    console.log("Received request from client");
+    
+    // Create response buffer (8 bytes total)
+    const response = Buffer.alloc(8);
+    
+    // Write message_size (4 bytes, big-endian) - value: 0
+    response.writeInt32BE(0, 0);
+    
+    // Write correlation_id (4 bytes, big-endian) - value: 7
+    response.writeInt32BE(7, 4);
+    
+    console.log("Sending response:", response.toString('hex'));
+    connection.write(response);
+  });
+  
+  connection.on("end", () => {
+    console.log("Client disconnected");
+  });
 });
 
-server.listen(9092, "127.0.0.1");
+server.listen(9092, "127.0.0.1", () => {
+  console.log("Kafka broker listening on port 9092");
+});
