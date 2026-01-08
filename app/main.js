428d1127484a4f7c3745906080e1fa4b6cdaa120
Zahdidmohd <zahidkhanavi@gmail.com>
2026-01-08 22:05:36 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -8,6 +8,16 @@ const server = net.createServer((connection) => {
   
   connection.on("data", (data) => {
     console.log("Received request from client");
+    console.log("Request data:", data.toString('hex'));
+    
+    // Parse request header v2
+    // Offset 0: message_size (4 bytes)
+    // Offset 4: request_api_key (2 bytes)
+    // Offset 6: request_api_version (2 bytes)
+    // Offset 8: correlation_id (4 bytes) <- We need this!
+    
+    const correlationId = data.readInt32BE(8);
+    console.log("Extracted correlation_id:", correlationId);
     
     // Create response buffer (8 bytes total)
     const response = Buffer.alloc(8);
@@ -15,8 +25,8 @@ const server = net.createServer((connection) => {
     // Write message_size (4 bytes, big-endian) - value: 0
     response.writeInt32BE(0, 0);
     
-    // Write correlation_id (4 bytes, big-endian) - value: 7
-    response.writeInt32BE(7, 4);
+    // Write correlation_id (4 bytes, big-endian) - echo back from request
+    response.writeInt32BE(correlationId, 4);
     
     console.log("Sending response:", response.toString('hex'));
     connection.write(response);
